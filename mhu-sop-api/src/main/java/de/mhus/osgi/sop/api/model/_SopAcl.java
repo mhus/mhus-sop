9074ab6869264909c4bc7cb8581824b2aa8f4067
Mike Hummel <>
2019-04-01 23:33:04 +0200

    version 1.6.0

--- mhu-sop-api/src/main/java/de/mhus/osgi/sop/api/model/_SopAcl.java
+++ mhu-sop-api/src/main/java/de/mhus/osgi/sop/api/model/_SopAcl.java
@@ -5,59 +5,58 @@ import de.mhus.lib.basics.consts.ConstBase;
 
 /**
  * File created by mhu const generator. Changes will be overwritten.
- * 28.2.2019
  **/
 
 public class _SopAcl extends ConstBase {
 
-  public static final Identifier CLASS_EXTENDS = new Identifier(de.mhus.osgi.sop.api.model.SopAcl.class,"de.mhus.lib.adb.DbMetadata");
-  public static final Identifier CLASS_NAME = new Identifier(de.mhus.osgi.sop.api.model.SopAcl.class,"de.mhus.osgi.sop.api.model.SopAcl");
-  public static final Identifier CLASS_PATH = new Identifier(de.mhus.osgi.sop.api.model.SopAcl.class,"de.mhus.osgi.sop.api.model.SopAcl");
-  public static final Identifier METHOD_CREATE = new Identifier(de.mhus.osgi.sop.api.model.SopAcl.class,"create");
-  public static final Identifier METHOD_DELETE = new Identifier(de.mhus.osgi.sop.api.model.SopAcl.class,"delete");
-  public static final Identifier METHOD_FIND_PARENT_OBJECT = new Identifier(de.mhus.osgi.sop.api.model.SopAcl.class,"findParentObject");
-  public static final Identifier METHOD_GET_ACL = new Identifier(de.mhus.osgi.sop.api.model.SopAcl.class,"getAcl");
-  public static final Identifier METHOD_GET_CREATION_DATE = new Identifier(de.mhus.osgi.sop.api.model.SopAcl.class,"getCreationDate");
-  public static final Identifier METHOD_GET_DB_HANDLER = new Identifier(de.mhus.osgi.sop.api.model.SopAcl.class,"getDbHandler");
-  public static final Identifier METHOD_GET_ID = new Identifier(de.mhus.osgi.sop.api.model.SopAcl.class,"getId");
-  public static final Identifier METHOD_GET_LIST = new Identifier(de.mhus.osgi.sop.api.model.SopAcl.class,"getList");
-  public static final Identifier METHOD_GET_MODIFY_DATE = new Identifier(de.mhus.osgi.sop.api.model.SopAcl.class,"getModifyDate");
-  public static final Identifier METHOD_GET_TARGET = new Identifier(de.mhus.osgi.sop.api.model.SopAcl.class,"getTarget");
-  public static final Identifier METHOD_GET_VSTAMP = new Identifier(de.mhus.osgi.sop.api.model.SopAcl.class,"getVstamp");
-  public static final Identifier METHOD_IS_ADB_CHANGED = new Identifier(de.mhus.osgi.sop.api.model.SopAcl.class,"isAdbChanged");
-  public static final Identifier METHOD_IS_ADB_MANAGED = new Identifier(de.mhus.osgi.sop.api.model.SopAcl.class,"isAdbManaged");
-  public static final Identifier METHOD_IS_ADB_PERSISTENT = new Identifier(de.mhus.osgi.sop.api.model.SopAcl.class,"isAdbPersistent");
-  public static final Identifier METHOD_LOG = new Identifier(de.mhus.osgi.sop.api.model.SopAcl.class,"log");
-  public static final Identifier METHOD_RELOAD = new Identifier(de.mhus.osgi.sop.api.model.SopAcl.class,"reload");
-  public static final Identifier METHOD_SAVE = new Identifier(de.mhus.osgi.sop.api.model.SopAcl.class,"save");
-  public static final Identifier METHOD_SAVE_CHANGED = new Identifier(de.mhus.osgi.sop.api.model.SopAcl.class,"saveChanged");
-  public static final Identifier METHOD_SET_LIST = new Identifier(de.mhus.osgi.sop.api.model.SopAcl.class,"setList");
-  public static final Identifier PROJECT_ARTIFACT = new Identifier(de.mhus.osgi.sop.api.model.SopAcl.class,"mhu-sop-api");
-  public static final Identifier PROJECT_DESCRIPTION = new Identifier(de.mhus.osgi.sop.api.model.SopAcl.class,"A set of tools to improve OSGi frameworks and karaf");
-  public static final Identifier PROJECT_GROUP = new Identifier(de.mhus.osgi.sop.api.model.SopAcl.class,"de.mhus.osgi");
-  public static final Identifier PROJECT_VERSION = new Identifier(de.mhus.osgi.sop.api.model.SopAcl.class,"1.6.0-SNAPSHOT");
-  public static final Identifier _ACL = new Identifier(de.mhus.osgi.sop.api.model.SopAcl.class,"Acl");
-  public static final Identifier _ADB_CHANGED = new Identifier(de.mhus.osgi.sop.api.model.SopAcl.class,"AdbChanged");
-  public static final Identifier _ADB_MANAGED = new Identifier(de.mhus.osgi.sop.api.model.SopAcl.class,"AdbManaged");
-  public static final Identifier _ADB_PERSISTENT = new Identifier(de.mhus.osgi.sop.api.model.SopAcl.class,"AdbPersistent");
-  public static final Identifier _CON = new Identifier(de.mhus.osgi.sop.api.model.SopAcl.class,"con");
-  public static final Identifier _CREATE = new Identifier(de.mhus.osgi.sop.api.model.SopAcl.class,"create");
-  public static final Identifier _CREATION_DATE = new Identifier(de.mhus.osgi.sop.api.model.SopAcl.class,"CreationDate");
-  public static final Identifier _DB_HANDLER = new Identifier(de.mhus.osgi.sop.api.model.SopAcl.class,"DbHandler");
-  public static final Identifier _DELETE = new Identifier(de.mhus.osgi.sop.api.model.SopAcl.class,"delete");
-  public static final Identifier _FIND_PARENT_OBJECT = new Identifier(de.mhus.osgi.sop.api.model.SopAcl.class,"findParentObject");
-  public static final Identifier _ID = new Identifier(de.mhus.osgi.sop.api.model.SopAcl.class,"Id");
-  public static final Identifier _LIST = new Identifier(de.mhus.osgi.sop.api.model.SopAcl.class,"List");
-  public static final Identifier _LOG = new Identifier(de.mhus.osgi.sop.api.model.SopAcl.class,"log");
-  public static final Identifier _MANAGER = new Identifier(de.mhus.osgi.sop.api.model.SopAcl.class,"manager");
-  public static final Identifier _MODIFY_DATE = new Identifier(de.mhus.osgi.sop.api.model.SopAcl.class,"ModifyDate");
-  public static final Identifier _PERSISTENT = new Identifier(de.mhus.osgi.sop.api.model.SopAcl.class,"persistent");
-  public static final Identifier _REGISTER_NATIVES = new Identifier(de.mhus.osgi.sop.api.model.SopAcl.class,"registerNatives");
-  public static final Identifier _REGISTRY_NAME = new Identifier(de.mhus.osgi.sop.api.model.SopAcl.class,"registryName");
-  public static final Identifier _RELOAD = new Identifier(de.mhus.osgi.sop.api.model.SopAcl.class,"reload");
-  public static final Identifier _SAVE = new Identifier(de.mhus.osgi.sop.api.model.SopAcl.class,"save");
-  public static final Identifier _SAVE_CHANGED = new Identifier(de.mhus.osgi.sop.api.model.SopAcl.class,"saveChanged");
-  public static final Identifier _TARGET = new Identifier(de.mhus.osgi.sop.api.model.SopAcl.class,"Target");
-  public static final Identifier _VSTAMP = new Identifier(de.mhus.osgi.sop.api.model.SopAcl.class,"Vstamp");
+  public static final Identifier CLASS_EXTENDS = new Identifier(Identifier.TYPE.CLASS, de.mhus.osgi.sop.api.model.SopAcl.class,"de.mhus.lib.adb.DbMetadata");
+  public static final Identifier CLASS_NAME = new Identifier(Identifier.TYPE.CLASS, de.mhus.osgi.sop.api.model.SopAcl.class,"de.mhus.osgi.sop.api.model.SopAcl");
+  public static final Identifier CLASS_PATH = new Identifier(Identifier.TYPE.CLASS, de.mhus.osgi.sop.api.model.SopAcl.class,"de.mhus.osgi.sop.api.model.SopAcl");
+  public static final Identifier METHOD_CREATE = new Identifier(Identifier.TYPE.ACTION, de.mhus.osgi.sop.api.model.SopAcl.class,"create");
+  public static final Identifier METHOD_DELETE = new Identifier(Identifier.TYPE.ACTION, de.mhus.osgi.sop.api.model.SopAcl.class,"delete");
+  public static final Identifier METHOD_FIND_PARENT_OBJECT = new Identifier(Identifier.TYPE.ACTION, de.mhus.osgi.sop.api.model.SopAcl.class,"findParentObject");
+  public static final Identifier METHOD_GET_ACL = new Identifier(Identifier.TYPE.GETTER, de.mhus.osgi.sop.api.model.SopAcl.class,"getAcl");
+  public static final Identifier METHOD_GET_CREATION_DATE = new Identifier(Identifier.TYPE.GETTER, de.mhus.osgi.sop.api.model.SopAcl.class,"getCreationDate");
+  public static final Identifier METHOD_GET_DB_HANDLER = new Identifier(Identifier.TYPE.GETTER, de.mhus.osgi.sop.api.model.SopAcl.class,"getDbHandler");
+  public static final Identifier METHOD_GET_ID = new Identifier(Identifier.TYPE.GETTER, de.mhus.osgi.sop.api.model.SopAcl.class,"getId");
+  public static final Identifier METHOD_GET_LIST = new Identifier(Identifier.TYPE.GETTER, de.mhus.osgi.sop.api.model.SopAcl.class,"getList");
+  public static final Identifier METHOD_GET_MODIFY_DATE = new Identifier(Identifier.TYPE.GETTER, de.mhus.osgi.sop.api.model.SopAcl.class,"getModifyDate");
+  public static final Identifier METHOD_GET_TARGET = new Identifier(Identifier.TYPE.GETTER, de.mhus.osgi.sop.api.model.SopAcl.class,"getTarget");
+  public static final Identifier METHOD_GET_VSTAMP = new Identifier(Identifier.TYPE.GETTER, de.mhus.osgi.sop.api.model.SopAcl.class,"getVstamp");
+  public static final Identifier METHOD_IS_ADB_CHANGED = new Identifier(Identifier.TYPE.GETTER, de.mhus.osgi.sop.api.model.SopAcl.class,"isAdbChanged");
+  public static final Identifier METHOD_IS_ADB_MANAGED = new Identifier(Identifier.TYPE.GETTER, de.mhus.osgi.sop.api.model.SopAcl.class,"isAdbManaged");
+  public static final Identifier METHOD_IS_ADB_PERSISTENT = new Identifier(Identifier.TYPE.GETTER, de.mhus.osgi.sop.api.model.SopAcl.class,"isAdbPersistent");
+  public static final Identifier METHOD_LOG = new Identifier(Identifier.TYPE.ACTION, de.mhus.osgi.sop.api.model.SopAcl.class,"log");
+  public static final Identifier METHOD_RELOAD = new Identifier(Identifier.TYPE.ACTION, de.mhus.osgi.sop.api.model.SopAcl.class,"reload");
+  public static final Identifier METHOD_SAVE = new Identifier(Identifier.TYPE.ACTION, de.mhus.osgi.sop.api.model.SopAcl.class,"save");
+  public static final Identifier METHOD_SAVE_CHANGED = new Identifier(Identifier.TYPE.ACTION, de.mhus.osgi.sop.api.model.SopAcl.class,"saveChanged");
+  public static final Identifier METHOD_SET_LIST = new Identifier(Identifier.TYPE.SETTER, de.mhus.osgi.sop.api.model.SopAcl.class,"setList");
+  public static final Identifier PROJECT_ARTIFACT = new Identifier(Identifier.TYPE.MAVEN, de.mhus.osgi.sop.api.model.SopAcl.class,"mhu-sop-api");
+  public static final Identifier PROJECT_DESCRIPTION = new Identifier(Identifier.TYPE.MAVEN, de.mhus.osgi.sop.api.model.SopAcl.class,"A set of tools to improve OSGi frameworks and karaf");
+  public static final Identifier PROJECT_GROUP = new Identifier(Identifier.TYPE.MAVEN, de.mhus.osgi.sop.api.model.SopAcl.class,"de.mhus.osgi");
+  public static final Identifier PROJECT_VERSION = new Identifier(Identifier.TYPE.MAVEN, de.mhus.osgi.sop.api.model.SopAcl.class,"1.6.0");
+  public static final Identifier _ACL = new Identifier(Identifier.TYPE.GETTER, de.mhus.osgi.sop.api.model.SopAcl.class,"Acl");
+  public static final Identifier _ADB_CHANGED = new Identifier(Identifier.TYPE.GETTER, de.mhus.osgi.sop.api.model.SopAcl.class,"AdbChanged");
+  public static final Identifier _ADB_MANAGED = new Identifier(Identifier.TYPE.GETTER, de.mhus.osgi.sop.api.model.SopAcl.class,"AdbManaged");
+  public static final Identifier _ADB_PERSISTENT = new Identifier(Identifier.TYPE.GETTER, de.mhus.osgi.sop.api.model.SopAcl.class,"AdbPersistent");
+  public static final Identifier _CON = new Identifier(Identifier.TYPE.FIELD, de.mhus.osgi.sop.api.model.SopAcl.class,"con");
+  public static final Identifier _CREATE = new Identifier(Identifier.TYPE.ACTION, de.mhus.osgi.sop.api.model.SopAcl.class,"create");
+  public static final Identifier _CREATION_DATE = new Identifier(Identifier.TYPE.GETTER, de.mhus.osgi.sop.api.model.SopAcl.class,"CreationDate");
+  public static final Identifier _DB_HANDLER = new Identifier(Identifier.TYPE.GETTER, de.mhus.osgi.sop.api.model.SopAcl.class,"DbHandler");
+  public static final Identifier _DELETE = new Identifier(Identifier.TYPE.ACTION, de.mhus.osgi.sop.api.model.SopAcl.class,"delete");
+  public static final Identifier _FIND_PARENT_OBJECT = new Identifier(Identifier.TYPE.ACTION, de.mhus.osgi.sop.api.model.SopAcl.class,"findParentObject");
+  public static final Identifier _ID = new Identifier(Identifier.TYPE.GETTER, de.mhus.osgi.sop.api.model.SopAcl.class,"Id");
+  public static final Identifier _LIST = new Identifier(Identifier.TYPE.SETTER, de.mhus.osgi.sop.api.model.SopAcl.class,"List");
+  public static final Identifier _LOG = new Identifier(Identifier.TYPE.ACTION, de.mhus.osgi.sop.api.model.SopAcl.class,"log");
+  public static final Identifier _MANAGER = new Identifier(Identifier.TYPE.FIELD, de.mhus.osgi.sop.api.model.SopAcl.class,"manager");
+  public static final Identifier _MODIFY_DATE = new Identifier(Identifier.TYPE.GETTER, de.mhus.osgi.sop.api.model.SopAcl.class,"ModifyDate");
+  public static final Identifier _PERSISTENT = new Identifier(Identifier.TYPE.FIELD, de.mhus.osgi.sop.api.model.SopAcl.class,"persistent");
+  public static final Identifier _REGISTER_NATIVES = new Identifier(Identifier.TYPE.ACTION, de.mhus.osgi.sop.api.model.SopAcl.class,"registerNatives");
+  public static final Identifier _REGISTRY_NAME = new Identifier(Identifier.TYPE.FIELD, de.mhus.osgi.sop.api.model.SopAcl.class,"registryName");
+  public static final Identifier _RELOAD = new Identifier(Identifier.TYPE.ACTION, de.mhus.osgi.sop.api.model.SopAcl.class,"reload");
+  public static final Identifier _SAVE = new Identifier(Identifier.TYPE.ACTION, de.mhus.osgi.sop.api.model.SopAcl.class,"save");
+  public static final Identifier _SAVE_CHANGED = new Identifier(Identifier.TYPE.ACTION, de.mhus.osgi.sop.api.model.SopAcl.class,"saveChanged");
+  public static final Identifier _TARGET = new Identifier(Identifier.TYPE.GETTER, de.mhus.osgi.sop.api.model.SopAcl.class,"Target");
+  public static final Identifier _VSTAMP = new Identifier(Identifier.TYPE.GETTER, de.mhus.osgi.sop.api.model.SopAcl.class,"Vstamp");
 
 }
--- mhu-sop-api/src/main/java/de/mhus/osgi/sop/api/model/_SopRegister.java
+++ mhu-sop-api/src/main/java/de/mhus/osgi/sop/api/model/_SopRegister.java
@@ -5,58 +5,57 @@ import de.mhus.lib.basics.consts.ConstBase;
 
 /**
  * File created by mhu const generator. Changes will be overwritten.
- * 28.2.2019
  **/
 
 public class _SopRegister extends ConstBase {
 
-  public static final Identifier CLASS_EXTENDS = new Identifier(de.mhus.osgi.sop.api.model.SopRegister.class,"de.mhus.lib.adb.DbComfortableObject");
-  public static final Identifier CLASS_NAME = new Identifier(de.mhus.osgi.sop.api.model.SopRegister.class,"de.mhus.osgi.sop.api.model.SopRegister");
-  public static final Identifier CLASS_PATH = new Identifier(de.mhus.osgi.sop.api.model.SopRegister.class,"de.mhus.osgi.sop.api.model.SopRegister");
-  public static final Identifier METHOD_CREATE = new Identifier(de.mhus.osgi.sop.api.model.SopRegister.class,"create");
-  public static final Identifier METHOD_DELETE = new Identifier(de.mhus.osgi.sop.api.model.SopRegister.class,"delete");
-  public static final Identifier METHOD_GET_DB_HANDLER = new Identifier(de.mhus.osgi.sop.api.model.SopRegister.class,"getDbHandler");
-  public static final Identifier METHOD_GET_ID = new Identifier(de.mhus.osgi.sop.api.model.SopRegister.class,"getId");
-  public static final Identifier METHOD_GET_KEY1 = new Identifier(de.mhus.osgi.sop.api.model.SopRegister.class,"getKey1");
-  public static final Identifier METHOD_GET_KEY2 = new Identifier(de.mhus.osgi.sop.api.model.SopRegister.class,"getKey2");
-  public static final Identifier METHOD_GET_NAME = new Identifier(de.mhus.osgi.sop.api.model.SopRegister.class,"getName");
-  public static final Identifier METHOD_GET_VALUE1 = new Identifier(de.mhus.osgi.sop.api.model.SopRegister.class,"getValue1");
-  public static final Identifier METHOD_GET_VALUE2 = new Identifier(de.mhus.osgi.sop.api.model.SopRegister.class,"getValue2");
-  public static final Identifier METHOD_IS_ADB_CHANGED = new Identifier(de.mhus.osgi.sop.api.model.SopRegister.class,"isAdbChanged");
-  public static final Identifier METHOD_IS_ADB_MANAGED = new Identifier(de.mhus.osgi.sop.api.model.SopRegister.class,"isAdbManaged");
-  public static final Identifier METHOD_IS_ADB_PERSISTENT = new Identifier(de.mhus.osgi.sop.api.model.SopRegister.class,"isAdbPersistent");
-  public static final Identifier METHOD_LOG = new Identifier(de.mhus.osgi.sop.api.model.SopRegister.class,"log");
-  public static final Identifier METHOD_RELOAD = new Identifier(de.mhus.osgi.sop.api.model.SopRegister.class,"reload");
-  public static final Identifier METHOD_SAVE = new Identifier(de.mhus.osgi.sop.api.model.SopRegister.class,"save");
-  public static final Identifier METHOD_SAVE_CHANGED = new Identifier(de.mhus.osgi.sop.api.model.SopRegister.class,"saveChanged");
-  public static final Identifier METHOD_SET_KEY1 = new Identifier(de.mhus.osgi.sop.api.model.SopRegister.class,"setKey1");
-  public static final Identifier METHOD_SET_KEY2 = new Identifier(de.mhus.osgi.sop.api.model.SopRegister.class,"setKey2");
-  public static final Identifier METHOD_SET_VALUE1 = new Identifier(de.mhus.osgi.sop.api.model.SopRegister.class,"setValue1");
-  public static final Identifier METHOD_SET_VALUE2 = new Identifier(de.mhus.osgi.sop.api.model.SopRegister.class,"setValue2");
-  public static final Identifier PROJECT_ARTIFACT = new Identifier(de.mhus.osgi.sop.api.model.SopRegister.class,"mhu-sop-api");
-  public static final Identifier PROJECT_DESCRIPTION = new Identifier(de.mhus.osgi.sop.api.model.SopRegister.class,"A set of tools to improve OSGi frameworks and karaf");
-  public static final Identifier PROJECT_GROUP = new Identifier(de.mhus.osgi.sop.api.model.SopRegister.class,"de.mhus.osgi");
-  public static final Identifier PROJECT_VERSION = new Identifier(de.mhus.osgi.sop.api.model.SopRegister.class,"1.6.0-SNAPSHOT");
-  public static final Identifier _ADB_CHANGED = new Identifier(de.mhus.osgi.sop.api.model.SopRegister.class,"AdbChanged");
-  public static final Identifier _ADB_MANAGED = new Identifier(de.mhus.osgi.sop.api.model.SopRegister.class,"AdbManaged");
-  public static final Identifier _ADB_PERSISTENT = new Identifier(de.mhus.osgi.sop.api.model.SopRegister.class,"AdbPersistent");
-  public static final Identifier _CON = new Identifier(de.mhus.osgi.sop.api.model.SopRegister.class,"con");
-  public static final Identifier _CREATE = new Identifier(de.mhus.osgi.sop.api.model.SopRegister.class,"create");
-  public static final Identifier _DB_HANDLER = new Identifier(de.mhus.osgi.sop.api.model.SopRegister.class,"DbHandler");
-  public static final Identifier _DELETE = new Identifier(de.mhus.osgi.sop.api.model.SopRegister.class,"delete");
-  public static final Identifier _ID = new Identifier(de.mhus.osgi.sop.api.model.SopRegister.class,"Id");
-  public static final Identifier _KEY1 = new Identifier(de.mhus.osgi.sop.api.model.SopRegister.class,"Key1");
-  public static final Identifier _KEY2 = new Identifier(de.mhus.osgi.sop.api.model.SopRegister.class,"Key2");
-  public static final Identifier _LOG = new Identifier(de.mhus.osgi.sop.api.model.SopRegister.class,"log");
-  public static final Identifier _MANAGER = new Identifier(de.mhus.osgi.sop.api.model.SopRegister.class,"manager");
-  public static final Identifier _NAME = new Identifier(de.mhus.osgi.sop.api.model.SopRegister.class,"Name");
-  public static final Identifier _PERSISTENT = new Identifier(de.mhus.osgi.sop.api.model.SopRegister.class,"persistent");
-  public static final Identifier _REGISTER_NATIVES = new Identifier(de.mhus.osgi.sop.api.model.SopRegister.class,"registerNatives");
-  public static final Identifier _REGISTRY_NAME = new Identifier(de.mhus.osgi.sop.api.model.SopRegister.class,"registryName");
-  public static final Identifier _RELOAD = new Identifier(de.mhus.osgi.sop.api.model.SopRegister.class,"reload");
-  public static final Identifier _SAVE = new Identifier(de.mhus.osgi.sop.api.model.SopRegister.class,"save");
-  public static final Identifier _SAVE_CHANGED = new Identifier(de.mhus.osgi.sop.api.model.SopRegister.class,"saveChanged");
-  public static final Identifier _VALUE1 = new Identifier(de.mhus.osgi.sop.api.model.SopRegister.class,"Value1");
-  public static final Identifier _VALUE2 = new Identifier(de.mhus.osgi.sop.api.model.SopRegister.class,"Value2");
+  public static final Identifier CLASS_EXTENDS = new Identifier(Identifier.TYPE.CLASS, de.mhus.osgi.sop.api.model.SopRegister.class,"de.mhus.lib.adb.DbComfortableObject");
+  public static final Identifier CLASS_NAME = new Identifier(Identifier.TYPE.CLASS, de.mhus.osgi.sop.api.model.SopRegister.class,"de.mhus.osgi.sop.api.model.SopRegister");
+  public static final Identifier CLASS_PATH = new Identifier(Identifier.TYPE.CLASS, de.mhus.osgi.sop.api.model.SopRegister.class,"de.mhus.osgi.sop.api.model.SopRegister");
+  public static final Identifier METHOD_CREATE = new Identifier(Identifier.TYPE.ACTION, de.mhus.osgi.sop.api.model.SopRegister.class,"create");
+  public static final Identifier METHOD_DELETE = new Identifier(Identifier.TYPE.ACTION, de.mhus.osgi.sop.api.model.SopRegister.class,"delete");
+  public static final Identifier METHOD_GET_DB_HANDLER = new Identifier(Identifier.TYPE.GETTER, de.mhus.osgi.sop.api.model.SopRegister.class,"getDbHandler");
+  public static final Identifier METHOD_GET_ID = new Identifier(Identifier.TYPE.GETTER, de.mhus.osgi.sop.api.model.SopRegister.class,"getId");
+  public static final Identifier METHOD_GET_KEY1 = new Identifier(Identifier.TYPE.GETTER, de.mhus.osgi.sop.api.model.SopRegister.class,"getKey1");
+  public static final Identifier METHOD_GET_KEY2 = new Identifier(Identifier.TYPE.GETTER, de.mhus.osgi.sop.api.model.SopRegister.class,"getKey2");
+  public static final Identifier METHOD_GET_NAME = new Identifier(Identifier.TYPE.GETTER, de.mhus.osgi.sop.api.model.SopRegister.class,"getName");
+  public static final Identifier METHOD_GET_VALUE1 = new Identifier(Identifier.TYPE.GETTER, de.mhus.osgi.sop.api.model.SopRegister.class,"getValue1");
+  public static final Identifier METHOD_GET_VALUE2 = new Identifier(Identifier.TYPE.GETTER, de.mhus.osgi.sop.api.model.SopRegister.class,"getValue2");
+  public static final Identifier METHOD_IS_ADB_CHANGED = new Identifier(Identifier.TYPE.GETTER, de.mhus.osgi.sop.api.model.SopRegister.class,"isAdbChanged");
+  public static final Identifier METHOD_IS_ADB_MANAGED = new Identifier(Identifier.TYPE.GETTER, de.mhus.osgi.sop.api.model.SopRegister.class,"isAdbManaged");
+  public static final Identifier METHOD_IS_ADB_PERSISTENT = new Identifier(Identifier.TYPE.GETTER, de.mhus.osgi.sop.api.model.SopRegister.class,"isAdbPersistent");
+  public static final Identifier METHOD_LOG = new Identifier(Identifier.TYPE.ACTION, de.mhus.osgi.sop.api.model.SopRegister.class,"log");
+  public static final Identifier METHOD_RELOAD = new Identifier(Identifier.TYPE.ACTION, de.mhus.osgi.sop.api.model.SopRegister.class,"reload");
+  public static final Identifier METHOD_SAVE = new Identifier(Identifier.TYPE.ACTION, de.mhus.osgi.sop.api.model.SopRegister.class,"save");
+  public static final Identifier METHOD_SAVE_CHANGED = new Identifier(Identifier.TYPE.ACTION, de.mhus.osgi.sop.api.model.SopRegister.class,"saveChanged");
+  public static final Identifier METHOD_SET_KEY1 = new Identifier(Identifier.TYPE.SETTER, de.mhus.osgi.sop.api.model.SopRegister.class,"setKey1");
+  public static final Identifier METHOD_SET_KEY2 = new Identifier(Identifier.TYPE.SETTER, de.mhus.osgi.sop.api.model.SopRegister.class,"setKey2");
+  public static final Identifier METHOD_SET_VALUE1 = new Identifier(Identifier.TYPE.SETTER, de.mhus.osgi.sop.api.model.SopRegister.class,"setValue1");
+  public static final Identifier METHOD_SET_VALUE2 = new Identifier(Identifier.TYPE.SETTER, de.mhus.osgi.sop.api.model.SopRegister.class,"setValue2");
+  public static final Identifier PROJECT_ARTIFACT = new Identifier(Identifier.TYPE.MAVEN, de.mhus.osgi.sop.api.model.SopRegister.class,"mhu-sop-api");
+  public static final Identifier PROJECT_DESCRIPTION = new Identifier(Identifier.TYPE.MAVEN, de.mhus.osgi.sop.api.model.SopRegister.class,"A set of tools to improve OSGi frameworks and karaf");
+  public static final Identifier PROJECT_GROUP = new Identifier(Identifier.TYPE.MAVEN, de.mhus.osgi.sop.api.model.SopRegister.class,"de.mhus.osgi");
+  public static final Identifier PROJECT_VERSION = new Identifier(Identifier.TYPE.MAVEN, de.mhus.osgi.sop.api.model.SopRegister.class,"1.6.0");
+  public static final Identifier _ADB_CHANGED = new Identifier(Identifier.TYPE.GETTER, de.mhus.osgi.sop.api.model.SopRegister.class,"AdbChanged");
+  public static final Identifier _ADB_MANAGED = new Identifier(Identifier.TYPE.GETTER, de.mhus.osgi.sop.api.model.SopRegister.class,"AdbManaged");
+  public static final Identifier _ADB_PERSISTENT = new Identifier(Identifier.TYPE.GETTER, de.mhus.osgi.sop.api.model.SopRegister.class,"AdbPersistent");
+  public static final Identifier _CON = new Identifier(Identifier.TYPE.FIELD, de.mhus.osgi.sop.api.model.SopRegister.class,"con");
+  public static final Identifier _CREATE = new Identifier(Identifier.TYPE.ACTION, de.mhus.osgi.sop.api.model.SopRegister.class,"create");
+  public static final Identifier _DB_HANDLER = new Identifier(Identifier.TYPE.GETTER, de.mhus.osgi.sop.api.model.SopRegister.class,"DbHandler");
+  public static final Identifier _DELETE = new Identifier(Identifier.TYPE.ACTION, de.mhus.osgi.sop.api.model.SopRegister.class,"delete");
+  public static final Identifier _ID = new Identifier(Identifier.TYPE.GETTER, de.mhus.osgi.sop.api.model.SopRegister.class,"Id");
+  public static final Identifier _KEY1 = new Identifier(Identifier.TYPE.SETTER, de.mhus.osgi.sop.api.model.SopRegister.class,"Key1");
+  public static final Identifier _KEY2 = new Identifier(Identifier.TYPE.GETTER, de.mhus.osgi.sop.api.model.SopRegister.class,"Key2");
+  public static final Identifier _LOG = new Identifier(Identifier.TYPE.ACTION, de.mhus.osgi.sop.api.model.SopRegister.class,"log");
+  public static final Identifier _MANAGER = new Identifier(Identifier.TYPE.FIELD, de.mhus.osgi.sop.api.model.SopRegister.class,"manager");
+  public static final Identifier _NAME = new Identifier(Identifier.TYPE.GETTER, de.mhus.osgi.sop.api.model.SopRegister.class,"Name");
+  public static final Identifier _PERSISTENT = new Identifier(Identifier.TYPE.FIELD, de.mhus.osgi.sop.api.model.SopRegister.class,"persistent");
+  public static final Identifier _REGISTER_NATIVES = new Identifier(Identifier.TYPE.ACTION, de.mhus.osgi.sop.api.model.SopRegister.class,"registerNatives");
+  public static final Identifier _REGISTRY_NAME = new Identifier(Identifier.TYPE.FIELD, de.mhus.osgi.sop.api.model.SopRegister.class,"registryName");
+  public static final Identifier _RELOAD = new Identifier(Identifier.TYPE.ACTION, de.mhus.osgi.sop.api.model.SopRegister.class,"reload");
+  public static final Identifier _SAVE = new Identifier(Identifier.TYPE.ACTION, de.mhus.osgi.sop.api.model.SopRegister.class,"save");
+  public static final Identifier _SAVE_CHANGED = new Identifier(Identifier.TYPE.ACTION, de.mhus.osgi.sop.api.model.SopRegister.class,"saveChanged");
+  public static final Identifier _VALUE1 = new Identifier(Identifier.TYPE.GETTER, de.mhus.osgi.sop.api.model.SopRegister.class,"Value1");
+  public static final Identifier _VALUE2 = new Identifier(Identifier.TYPE.SETTER, de.mhus.osgi.sop.api.model.SopRegister.class,"Value2");
 
 }
