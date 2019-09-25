bafc916e1f97e67da8d7f77dcc61a6076228555b
Mike Hummel <>
2019-09-25 10:53:54 +0200

    Create SuccessfulJsonResult.java

---
+++ mhu-sop-api/src/main/java/de/mhus/osgi/sop/api/rest/SuccessfulJsonResult.java
@@ -0,0 +1,22 @@
+package de.mhus.osgi.sop.api.rest;
+
+import org.codehaus.jackson.node.ObjectNode;
+
+import de.mhus.osgi.sop.api.rest.JsonResult;
+
+public class SuccessfulJsonResult extends JsonResult {
+
+    private ObjectNode data;
+
+    public SuccessfulJsonResult(String msg) {
+        data = createObjectNode();
+        data.put("successful", true);
+        data.put("rc", 0);
+        if (msg != null)
+            data.put("msg", msg);
+    }
+
+    public ObjectNode getData() {
+        return data;
+    }
+}
